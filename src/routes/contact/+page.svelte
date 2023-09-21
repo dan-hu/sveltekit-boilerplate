da214c1bbb53fc7f1a3b40cc4ac6d7cca20b0212
Dan Hu <hu1980@gmail.com>
2023-09-22 03:41:56 +0800

    Add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Contact Us</h1>
+
+	<p>
+		Ways to reach us.
+	</p>
+
+</div>
